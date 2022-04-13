51a8e6ec679eb1443d33dc170fa135c9eacfcca4
LEINAD <leinadser@gmail.com>
2022-04-13 19:58:21 -0300

    deployment

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '66941a491ba96ba71de3cdb05477a287'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '70e2d446-44aa-4445-9db9-fd1d9c23cd01'
+};
